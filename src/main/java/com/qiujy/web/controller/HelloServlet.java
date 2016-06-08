92562996d5773cae93ebc4d343e69f1266562eb1
zzc <zzc4cijenkins@126.com>
2016-06-08 09:38:28 +0800

    first commit

---
+++ src/main/java/com/qiujy/web/controller/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.qiujy.web.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("msg", "你好，第一个maven web项目");
+		
+		request.getRequestDispatcher("/hello.jsp").forward(request, response);
+	}
+
+}
